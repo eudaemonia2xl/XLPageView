b2218f7201ba6a5730bc68da58dcfcf85638370c
郑雪利 <1187896284@qq.com>
2018-01-02 17:50:46 +0800

    添加注释

--- XLPageView/XLPageView/XLPageView/XLSegmentConfig.h
+++ XLPageView/XLPageView/XLPageView/XLSegmentConfig.h
@@ -10,13 +10,15 @@
 #import <UIKit/UIKit.h>
 
 @interface XLSegmentConfig : NSObject
-
+/** 标题数组 */
 @property (nonatomic,strong)NSArray *titles;
+/** 选中颜色 */
 @property (nonatomic,strong)UIColor *selectedColor;
-
+/** 正常颜色 */
 @property (nonatomic,strong)UIColor *normalColor;
-
+/** 标记得高度 */
 @property (nonatomic,assign)CGFloat indicatorH;
+/** 标记得宽度 */
 @property (nonatomic,assign)CGFloat indicatorW;
 
 - (instancetype)initWithTitles:(NSArray *)titles pageWidth:(CGFloat)pageWidth;
